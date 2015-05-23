be5bcf1e4a2a713b74d4f8223a2245027fcb1dc8
Tobias Gassmann <tobias@gaszmann.de>
2015-05-23 20:53:13 +0000

    preparing for packagist

---
+++ src/CacheClient.php
@@ -0,0 +1,36 @@
+<?php
+namespace ReverseGeocoderCache;
+
+class CacheClient
+{
+  public function __construct($options){
+    $this->cacheFrontEnd = new CacheFrontEnd(
+      $options['cacheBackend'], 
+      $options['keySize'],
+      $options['prefix']
+      );
+      
+    $this->dataProvider = $options['dataProvider'];  
+  }
+  
+  
+  public function get($latitude, $longitude){
+    if (!$this->cachFrontEnd->exists($latitude,$longitude))
+    {
+      $data = $this->retrieveData($latitude, $longitude);
+      $this->cacheFrontEnd->set($latitude, $longitude, $data);
+      return $data;
+    }
+    else 
+    {
+      return $this->cachFrontEnd->get($latitude, $longitude);  
+    }
+  }
+  
+  protected function retrieveData($latitude, $longitude)
+  {
+    return $this->dataProvider->retrieveData($latitude, $longitude);
+  }
+  
+  
+}
