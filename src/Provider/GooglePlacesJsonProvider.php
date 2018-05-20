8929052172c53eef598538315e0f73ae608b9938
root <root@AB9EFA7>
2018-05-20 10:21:28 +0000

    work

---
+++ src/Provider/GooglePlacesJsonProvider.php
@@ -0,0 +1,35 @@
+<?php
+namespace ReverseGeocoderCache\Provider;
+
+class GooglePlacesJsonProvider
+{
+  
+  public function setApiKey($apiKey)
+  {
+    $this->apiKey = $apiKey;
+  }
+  
+  public function setLanguage($lang)
+  {
+    $this->language = $lang;
+  }
+
+  protected function getLanguage()
+  {
+    return $this->language;
+    //$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+    //return $lang;
+  }
+
+  public function retrieveData($latitude, $longitude)
+  {
+    $url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=".$latitude.",".$longitude."&language=".$this->getLanguage()."&sensor=true&key=".$this->apiKey;
+    $dataString = @file_get_contents($url);
+    $data = json_decode($dataString,true);
+    return json_encode($data['results'][0]);
+
+    
+  }
+  
+  
+}
