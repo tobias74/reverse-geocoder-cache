561ad1c62ec9d0389de439edfe881e0fa34206b5
root <root@AB9EFA7>
2020-06-19 15:46:07 +0000

    add test framework

---
+++ tests/TileTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+final class TileTest extends TestCase
+{
+    public function testSanity(): void
+    {
+        $this->assertEquals(true, true);
+    }
+}
