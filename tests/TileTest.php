796fe300a9f8d85550350b90aa69d065e1df5d88
root <root@AB9EFA7>
2020-06-19 17:06:23 +0000

    add tests

--- tests/TileTest.php
+++ tests/TileTest.php
@@ -5,8 +5,41 @@ use PHPUnit\Framework\TestCase;
 
 final class TileTest extends TestCase
 {
+    protected function getMethod($name)
+    {
+        $class = new ReflectionClass(\ReverseGeocoderCache\CacheFrontEnd::class);
+        $method = $class->getMethod($name);
+        $method->setAccessible(true);
+
+        return $method;
+    }
+
     public function testSanity(): void
     {
         $this->assertEquals(true, true);
+    }
+
+    public function testSameTileHit(): void
+    {
+        $frontEnd = new \ReverseGeocoderCache\CacheFrontEnd();
+        $frontEnd->setKeySize(2000);
+        $getCacheKeyMethod = $this->getMethod('getCacheKey');
+
+        $cacheKeyBridge_A = $getCacheKeyMethod->invokeArgs($frontEnd, array(40.7080078, -73.9998796));
+        $cacheKeyBridge_B = $getCacheKeyMethod->invokeArgs($frontEnd, array(40.7037924, -73.9942927));
+
+        $this->assertEquals($cacheKeyBridge_A, $cacheKeyBridge_B);
+    }
+
+    public function testDifferentTileHit(): void
+    {
+        $frontEnd = new \ReverseGeocoderCache\CacheFrontEnd();
+        $frontEnd->setKeySize(1000);
+        $getCacheKeyMethod = $this->getMethod('getCacheKey');
+
+        $cacheKeyBridge_A = $getCacheKeyMethod->invokeArgs($frontEnd, array(40.7080078, -73.9998796));
+        $cacheKeyBridge_B = $getCacheKeyMethod->invokeArgs($frontEnd, array(40.7037924, -73.9942927));
+
+        $this->assertNotEquals($cacheKeyBridge_A, $cacheKeyBridge_B);
     }
 }
