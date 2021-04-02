ba8211cc5ab08e7a5d9645fe61ccdd4c2f5ba3ea
root <root@AB9EFA7>
2021-04-02 12:47:29 +0000

    resolve conflicts3

--- src/CacheClient.php
+++ src/CacheClient.php
@@ -4,50 +4,6 @@ namespace ReverseGeocoderCache;
 
 class CacheClient
 {
-<<<<<<< HEAD
-    protected static $countCacheMisses = 0;
-    protected $cacheFrontEnd = false;
-    protected $dataProvider = false;
-
-    public function setDataProvider($dataProvider)
-    {
-        $this->dataProvider = $dataProvider;
-    }
-
-    public function setCacheFrontEnd($frontEnd)
-    {
-        $this->cacheFrontEnd = $frontEnd;
-    }
-
-    public function get($latitude, $longitude)
-    {
-        if (!$this->cacheFrontEnd->exists($latitude, $longitude)) {
-            ++self::$countCacheMisses;
-            error_log('We had '.self::$countCacheMisses.' Cache-Misses so far...');
-            try {
-                $data = $this->retrieveData($latitude, $longitude);
-            } catch (\ErrorException $e) {
-                error_log('Google-Places-Error: ');
-                error_log($e->getMessage());
-                $data = 'place-error';
-            }
-
-            $this->cacheFrontEnd->set($latitude, $longitude, $data);
-
-            if ('place-error' === $data) {
-                throw new \Exception('we did get a place-error from google');
-            } else {
-                return $data;
-            }
-        } else {
-            $value = $this->cacheFrontEnd->get($latitude, $longitude);
-            if ('place-error' === $value) {
-                throw new \Exception('We had stored a place-error in the cache client');
-            } else {
-                return $value;
-            }
-        }
-=======
     protected $cacheFrontEnd = false;
     protected $dataProvider = false;
 
@@ -108,7 +64,6 @@ class CacheClient
     protected function getData($latitude, $longitude)
     {
         return json_decode($this->cacheFrontEnd->get($latitude, $longitude), true);
->>>>>>> f85e8b966b394df406e08af679c6af78e5d7df45
     }
 
     protected function retrieveData($latitude, $longitude)
