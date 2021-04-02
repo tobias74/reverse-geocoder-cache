cad15b04bb14a630de86a7e8eea2459a8aac1f0a
root <root@AB9EFA7>
2021-04-02 12:41:19 +0000

    fix

--- src/CacheClient.php
+++ src/CacheClient.php
@@ -1,64 +1,55 @@
 <?php
+
 namespace ReverseGeocoderCache;
 
 class CacheClient
 {
-  protected $cacheFrontEnd = false;
-  protected $dataProvider = false;
-  
+    protected static $countCacheMisses = 0;
+    protected $cacheFrontEnd = false;
+    protected $dataProvider = false;
 
-  public function setDataProvider($dataProvider)
-  {
-    $this->dataProvider = $dataProvider;
-  }
-  
-  public function setCacheFrontEnd($frontEnd)
-  {
-    $this->cacheFrontEnd = $frontEnd;
-  }
-  
-  
-  public function get($latitude, $longitude){
-    if (!$this->cacheFrontEnd->exists($latitude,$longitude))
+    public function setDataProvider($dataProvider)
     {
-      try
-      {
-        $data = $this->retrieveData($latitude, $longitude);
-      }
-      catch (\ErrorException $e)
-      {
-        $data = 'place-error';        
-      }
+        $this->dataProvider = $dataProvider;
+    }
 
-      $this->cacheFrontEnd->set($latitude, $longitude, $data);
-      
-      if ($data === 'place-error')
-      {
-        throw new \Exception('we did get a place-error from google');        
-      }
-      else
-      {
-        return $data;
-      }
-    }
-    else 
+    public function setCacheFrontEnd($frontEnd)
     {
-      $value = $this->cacheFrontEnd->get($latitude, $longitude);
-      if ($value === 'place-error')
-      {
-        throw new \Exception('We had stored a place-error in the cache client');
-      }
-      else
-      {
-        return $value;
-      }
+        $this->cacheFrontEnd = $frontEnd;
     }
-  }
-  
-  protected function retrieveData($latitude, $longitude)
-  {
-    return $this->dataProvider->retrieveData($latitude, $longitude);
-  }
-  
-  
+
+    public function get($latitude, $longitude)
+    {
+        if (!$this->cacheFrontEnd->exists($latitude, $longitude)) {
+            ++self::$countCacheMisses;
+            error_log('We had '.self::$countCacheMisses.' Cache-Misses so far...');
+            try {
+                $data = $this->retrieveData($latitude, $longitude);
+            } catch (\ErrorException $e) {
+                error_log('Google-Places-Error: ');
+                error_log($e->getMessage());
+                $data = 'place-error';
+            }
+
+            $this->cacheFrontEnd->set($latitude, $longitude, $data);
+
+            if ('place-error' === $data) {
+                throw new \Exception('we did get a place-error from google');
+            } else {
+                return $data;
+            }
+        } else {
+            $value = $this->cacheFrontEnd->get($latitude, $longitude);
+            if ('place-error' === $value) {
+                throw new \Exception('We had stored a place-error in the cache client');
+            } else {
+                return $value;
+            }
+        }
+    }
+
+    protected function retrieveData($latitude, $longitude)
+    {
+        return $this->dataProvider->retrieveData($latitude, $longitude);
+    }
 }
